4852e36d31144cfb3156ddc070404d94196cca9f
Matt Plichta <bigcatplichta@gmail.com>
2019-08-29 23:17:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,8 +7,8 @@ def nyc_pigeon_organizer(data)
     v.each do | key, name_list |
       name_list.each do | name |
         reorg_hash[name] = {}
-        reorg_hash[name][k] = []
-        reorg_hash[name][k] = key.to_s
+        # reorg_hash[name][k] = []
+        reorg_hash[name][k] = [key.to_s]
         binding.pry
       end
     end
