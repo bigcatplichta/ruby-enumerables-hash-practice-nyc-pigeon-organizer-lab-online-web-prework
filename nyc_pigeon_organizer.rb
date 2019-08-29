541d4251bc14eb07d09fe399a33b9a76d1105217
Matt Plichta <bigcatplichta@gmail.com>
2019-08-29 23:51:27 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  reorg_hash = {}
+  
+  data.collect do | k, v |
+    v.each do | key, name_list |
+      name_list.each do | name |
+          reorg_hash[name] = {} unless reorg_hash[name]
+          if !reorg_hash[name][k]
+          reorg_hash[name][k] = [key.to_s] 
+          else 
+          reorg_hash[name][k].push(key.to_s)
+          end
+      end
+    end
+  end
+  reorg_hash
 end
